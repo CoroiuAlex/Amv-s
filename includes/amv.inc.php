f95f389a3e31d809d7130fbc9dabeddc05e971c3
Alex <coroiuac@gmail.com>
2018-01-21 10:29:14 -0500

    Update

---
+++ includes/amv.inc.php
@@ -0,0 +1,5 @@
+<?php
+
+$vid_1 = 'https://www.youtube-nocookie.com/embed/QYxpY-U37aY?rel=0&controls=1&disablekb=1&modestbranding=1&showinfo=0&iv_load_policy=3&fs=0';
+$vid_2 = 'https://www.youtube.com/embed/?index=0&listType=playlist&list=PLrR0UWVc3H19qjOsZ864Dw-KlrGPU7l7F&controls=1&modestbranding=1&iv_load_policy=3&rel=0'
+$vid_3 = 'https://www.youtube.com/embed/QYxpY-U37aY?playlist=AKE4jz-awyw,X1wVZ1lsqA8&controls=1&modestbranding=1&iv_load_policy=3&rel=0'
--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<?php
-
-
-
-
- ?>
-
 
 <!DOCTYPE html>
 <html>
@@ -12,103 +5,108 @@
   <title>Full Screen Landing Page</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="styles.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css">
-  <script type="text/javascript" src="Fixed.js"></script>
 </head>
 
 <body>
-  <!-- <h1 class="animated infinite bounce">Example</h1> -->
-<div class="intro">
+
+<!-- Login form -->
+
+<div id="Login">
   <section>
     <div class="inner">
       <div class="content">
-        <form class="login-box" action="includes/login.inc.php" method="POST">
-          <img src="img/avatar.png" class="avatar">
 
-          <p>Username</p>
-          <input type="text" name="uid" placeholder="Enter Username">
-          <p>Password</p>
-          <input type="password" name="pwd" placeholder="Enter Password">
-          <button type="submit" name="submit">Login</button>
+          <form id="login-box" action="includes/login.inc.php" method="POST">
+              <img src="img/avatar.png" class="avatar">
 
-          <a class="lilbtn" href="#">Lost your password?</a><br>
-          <a class="lilbtn" href="Signup.php">Don't have an account?</a>
-        </form>
+              <p>Username</p>
+              <input type="text" name="uid" placeholder="Enter Username">
+              <p>Password</p>
+              <input type="password" name="pwd" placeholder="Enter Password">
+
+              <button type="submit" name="submit">Login</button>
+
+              <a class="lilbtn" href="#">Lost your password?</a><br>
+              <a class="lilbtn" href="Signup.php">Don't have an account?</a>
+
+          </form>
       </div>
-</div>
-</div>
+    </div>
   </section>
+</div>
+
+
+<!-- Header form -->
+
 
 <div class"header">
+
   <ul class="nav" id="nav">
     <li><a href="#nav">Home</a></li>
-    <li><a href="#nav">About Me</a></li>
+    <li><a href="#nav">Amv's</a></li>
     <li><a href="#nav">Services</a></li>
     <li><a href="#nav">Work</a></li>
     <li><a href="#">Login</a></li>
-    <li><form action="includes/logout.inc.php" method="POST"><button class="logout" type="submit" name="submit">Logout</button></li>
-    </form>
+    <li><form action="includes/logout.inc.php" method="POST"><button class="logout" type="submit" name="submit">Logout</button></form></li>
   </ul>
+
   </div>
+<button id="roundButton">okok</button>
 
+
+
+
+
+  <script>
+  var roundButton = document.querySelector("#roundButton");
+  var flyoutMenu = document.querySelector("#Login");
+
+  roundButton.addEventListener("click", showMenu, false);
+  flyoutMenu.addEventListener("click", hideMenu, false);
+
+  function showMenu(e) {
+    flyoutMenu.classList.add("show");
+
+    document.body.style.overflow = "hidden";
+  }
+  function hideMenu(e) {
+    flyoutMenu.classList.remove("show");
+    flyoutMenu.classList.add("hidden");
+    e.stopPropagation();
+
+    document.body.style.overflow = "auto";
+  }
+
+  </script>
+
+
+
+
+
+<!-- <div id="idiv"><iframe id="iframe_main" width="560" height="315" src="https://www.youtube-nocookie.com/embed/QYxpY-U37aY?rel=0&controls=0&disablekb=1&modestbranding=1&showinfo=0&iv_load_policy=3&fs=0&autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen class="vid"></iframe></div> -->
 <div class="main-body">
+  <h1>heyyyyyyyyy</h1>
   <div class="main-txt">
+      <h2>Today's random pick</h2>
 
-  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-  Why do we use it?
-  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-
-  Where does it come from?
-  Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-  The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-  Why do we use it?
-  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-
-  Where does it come from?
-  Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-  The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-  Why do we use it?
-  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-
-  Where does it come from?
-  Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-  The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-  Why do we use it?
-  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-
-  Where does it come from?
-  Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-  The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-  <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/QYxpY-U37aY?rel=0&controls=1&disablekb=1&modestbranding=1&showinfo=0&iv_load_policy=3&fs=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen class="vid"></iframe>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/?index=0&listType=playlist&list=PLrR0UWVc3H19qjOsZ864Dw-KlrGPU7l7F&controls=1&modestbranding=1&iv_load_policy=3&rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/QYxpY-U37aY?playlist=AKE4jz-awyw,X1wVZ1lsqA8&controls=1&modestbranding=1&iv_load_policy=3&rel=0"frameborder="0" allowfullscreen></iframe>
 
 <?php
 $search = 'syrex';
+$vid_1 = 'https://www.youtube-nocookie.com/embed/QYxpY-U37aY?rel=0&controls=1&disablekb=1&modestbranding=1&showinfo=0&iv_load_policy=3&fs=0';
+$vid_2 = 'https://www.youtube.com/embed/?index=0&listType=playlist&list=PLrR0UWVc3H19qjOsZ864Dw-KlrGPU7l7F&controls=1&modestbranding=1&iv_load_policy=3&rel=0';
+$vid_3 = 'https://www.youtube.com/embed/QYxpY-U37aY?playlist=AKE4jz-awyw,X1wVZ1lsqA8&controls=1&modestbranding=1&iv_load_policy=3&rel=0';
 
-echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/?listType=search&list='.$search.'&controls=1&modestbranding=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+$mt_rand = 1;
 
-
-
-
-
-
-
+if ($mt_rand == 1) {
+  echo '<div id="div"><iframe id="iframe_sec" width="560" height="315" src="'.$vid_1.'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen class="vid"></iframe></div>';
+} elseif ($mt_rand == 2) {
+  echo '<iframe  width="560" height="315" src="'.$vid_2.'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen class="vid"></iframe>';
+} elseif ($mt_rand == 3) {
+  echo '<iframe width="560" height="315" src="'.$vid_3.'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen class="vid"></iframe>';
+} elseif ($mt_rand == 4) {
+  echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/?listType=search&list='.$search.'&controls=1&modestbranding=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+}
  ?>
 
 
@@ -182,7 +180,7 @@ echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/?listT
   <br>
   <br>
   <br>
-</div>
+  </div>
 </div>
 
 </body>
